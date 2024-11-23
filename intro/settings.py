8a1a9255e48694f6d0f2810552721c33447a703b
Denis Buzin <buzin.den@bk.ru>
2024-11-23 22:51:04 +0300

    Старт проекта "Портфолио на Django"

--- intro/settings.py
+++ intro/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'intro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': '310535',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
